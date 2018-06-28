bd2ec2e2931ecf8bbb0a3634e0e375a34bf5f278
gaozhaoxing <267132777@qq.com>
2018-06-28 19:43:45 +0800

    d

--- api-gateway/src/main/java/com/example/demo/ApiGateWayApplication.java
+++ api-gateway/src/main/java/com/example/demo/ApiGateWayApplication.java
@@ -1,15 +1,24 @@
 package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-@EnableZuulProxy
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients(basePackages = { "cn.ys", "com.example" })
+@EnableZuulProxy
 @SpringCloudApplication
 public class ApiGateWayApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ApiGateWayApplication.class).web(true).run(args);
+	}
+
+	@Bean
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties() {
+		return new ZuulProperties();
 	}
 }
