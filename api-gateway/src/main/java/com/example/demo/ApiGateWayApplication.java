07749a2676e8e435d78b6ecaaa25fba67736eec7
gaozhaoxing <267132777@qq.com>
2018-07-06 17:53:15 +0800

    aa

--- api-gateway/src/main/java/com/example/demo/ApiGateWayApplication.java
+++ api-gateway/src/main/java/com/example/demo/ApiGateWayApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringCloudApplication
 public class ApiGateWayApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ApiGateWayApplication.class).web(true).run(args);
+		SpringApplication.run(ApiGateWayApplication.class, args);
 	}
 
 	@Bean
